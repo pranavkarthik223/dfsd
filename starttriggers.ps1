58c99c52419776c122bb56c1f102e7c0224e6613
pranavkarthik223 <158459276+pranavkarthik223@users.noreply.github.com>
2025-02-19 20:58:42 +0530

    Create starttriggers.ps1

---
+++ starttriggers.ps1
@@ -0,0 +1,18 @@
+ $triggerNames = $(triggerlist)
+       
+      # Retrieve all triggers from the specified Synapse workspace
+      $triggersSynapse = Get-AzSynapseTrigger -WorkspaceName "apk"
+       
+      # Loop through the list of trigger names and stop each one
+      $triggerNames | ForEach-Object {
+          $triggerName = $_  # The current trigger name in the loop
+          $trigger = $triggersSynapse | Where-Object { $_.Name -eq $triggerName }
+      
+          if ($trigger) {
+      		write-host "$trigger.Name"
+              Stop-AzSynapseTrigger -WorkspaceName "apk" -Name $trigger.Name
+              Write-Host "Stopped trigger: $($trigger.Name)"
+          } else {
+              Write-Host "Trigger '$($_)' not found in the workspace."
+          }
+      }
